5b2dce5c97fe230459a5c4a49b5cc401a131239e
ibrahimakdas <iaibrahimakdas@gmail.com>
2020-10-13 19:30:58 +0300

    kal dedim yyaaaaaaa

--- src/test/java/hello.java
+++ src/test/java/hello.java
@@ -3,5 +3,9 @@ public class hello {
     public static void main(String[] args) {
 
         System.out.println("sill awesome hello");
+
+        System.out.println("hello again........");
+
+        System.out.println("sgfsjfutyıtuıyuhj");
     }
 }
